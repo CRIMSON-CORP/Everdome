904d6862d9876b3c76d345541f07414863f79df6
CRIMSON <olueatoworosanwo@gmail.com>
2022-02-24 00:52:47 +0100

    Initial Commit

---
+++ _next/static/chunks/pages/[slug]-490e1b661f11db1b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[219],{4008:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/[slug]",function(){return u(2001)}])}},function(n){n.O(0,[774,885,741,473,888,179],(function(){return _=4008,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
